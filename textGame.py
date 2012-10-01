1e18871dadce0b8b22da80cbe5f56ab697ffe22b
E. Troy Clement <eclement@nmsu.edu>
2012-10-01 00:34:27 -0600

    Created Player class
    Looking pretty sweet, made a player class and gave it a bunch of sweet
    funtions. The main loop now shows them off a bit automagically.
    Next moves: figure out how to get player to learn from conversations,
    this part might get tricky...

--- textGame.py
+++ textGame.py
@@ -8,8 +8,14 @@ class TextGame():
 
         #dummy Character
         self.char1 = Character("Tool", "He's an asshole, you can tell.")
-
         self.dahRoom.addChar(self.char1)
+
+        #Player
+        self.player = Player()
+
+        #Free topics
+        self.hate = Topic("Hate", "A terible emotion.", [])
+        self.love = Topic("Love", "A mythical emotion.", [])
         
     #Hold player focus
     #get input
@@ -43,10 +49,49 @@ class TextGame():
     def mainLoop(self):
 
         while True:
-            print(self.FOCUS.lookAt())
-            self.display(self.FOCUS)
-            move = self.getInput()
-            self.FOCUS.interact(move)
+            print(self.FOCUS.lookAt() + "\n")
+            #self.display(self.FOCUS)
+            #move = self.getInput()
+            #self.FOCUS.interact(move)
+            print("You know:")
+            self.player.listTopics()
+            if self.player.inLex("love"): print("You know something of love.")
+            else: print("You know nothing of love.")
+            if self.player.inLex("hate"): print("You know something of hate.")
+            else: print("You know nothing of hate.")
+            
+            print("\nNow you're learning some things...")
+            self.player.learn(self.hate)
+            self.player.learn(self.love)
+            print("\nNow you know:")
+            self.player.listTopics()
+            if self.player.inLex("love"): print("You know something of love.")
+            else: print("You know nothing of love.")
+            if self.player.inLex("hate"): print("You know something of hate.")
+            else: print("You know nothing of hate.")
+            
+            print("\nForgeting 'love'.")
+            self.player.forget("lOvE")
+            print("\nNow you know:")
+            self.player.listTopics()
+            if self.player.inLex("love"): print("You know something of love.")
+            else:print("You know nothing of love.")
+            if self.player.inLex("hate"): print("You know something of hate.")
+            else: print("You know nothing of hate.")
+            
+            print("\nForgeting 'hate'.")
+            self.player.forget("Hate")
+            print("\nNow you know:")
+            self.player.listTopics()
+            if self.player.inLex("love"): print("You know something of love.")
+            else: print("You know nothing of love.")
+            if self.player.inLex("hate"): print("You know something of hate.")
+            else: print("You know nothing of hate.")
+            
+            print("\nNow you are nothing.")
+            print("End of game.")
+            break
+            
             
 
 #class Exit():
@@ -94,7 +139,7 @@ class Character(Prop):
     def __init__(self, name, description):
         super().__init__(name, description)
         self.topics = {}
-        self.topics['Greeting'] = Topic("Greeting","Hi.",[])
+        self.topics['Greeting'] = Topic("Greeting","Hi",[])
         self.topics['Goodbye'] = Topic("Goodbye","Bye", [])
         self.topics['Unknown'] = Topic("Unknown","I don't know what you're talking about", [])
         self.topics['Chat'] = Topic("Chat","This is my standard response", [])
@@ -109,8 +154,8 @@ class Character(Prop):
     def interact(self):
         self.talk()
 
-    def talk(self): ## I copy pasted this method, it's fucked. Redo it.
-        print(self.name + ' says, "' + self.topics['Greeting'].response + '"\n')
+    def talk(self):
+        print(self.name + ' says, "' + self.topics['Greeting'].response + '."\n')
 
         while True:
             found = False
@@ -121,7 +166,7 @@ class Character(Prop):
             print(subject.lower() + "\n\n")
 
             if( subject.lower() == "leave"):
-                print('"'+ self.topics['Goodbye'].response + '"\n')
+                print('"'+ self.topics['Goodbye'].response + '."\n')
                 break
             
             if( subject.lower() == "look"):
@@ -130,15 +175,11 @@ class Character(Prop):
 
             for key in self.topics:
                 if subject.lower() == key.lower():
-                    print('"' + self.topics[key].response + '"\n')
+                    print('"' + self.topics[key].response + '."\n')
                     found = True
                     break
             if found == False: print('"' + self.topics["Unknown"].response + '"\n')
-        '''
-        Greeting
-        goodbye
-        unknown
-        '''
+
     #States
         '''
         Interact
@@ -150,12 +191,66 @@ class Character(Prop):
         inventory
         '''
 
+class Player():
+
+    def __init__(self):
+        self.lexicon = {}
+        self.inventory = {}
+
+    #--------- Item methods
+
+    def get(self, item):
+        self.inventory[item.name] = item
+
+    def lose(self, item):
+        item = item.capitalize() #Forces correct format
+        if item in self.invetory:
+            del self.inventory[item]
+
+    def listItems(self):
+        for key in self.inventory:
+            print(self.inventory[key].name)
+            
+    #Checks if item is in inventory returns true/false
+    def inInv(self, item):
+        item = item.capitalize()
+        for key in self.inventory:
+            if item == self.inventory[key].name: return True
+        return False
+
+    #--------- Topic methods
+
+    def learn(self, topic):
+        self.lexicon[topic.name] = topic
+
+    def forget(self, topic):
+        topic = topic.capitalize() #Forces correct format
+        if topic in self.lexicon:
+            del self.lexicon[topic]
+
+    def listTopics(self):
+        print("You've heard of:")
+        for key in self.lexicon:
+            print(self.lexicon[key].name)
+
+    def inLex(self, topic):
+        topic = topic.capitalize()
+        for key in self.lexicon:
+            if topic == self.lexicon[key].name: return True
+        return False
+
+    
+
 class Topic():
 
     def __init__(self, name, response, topics):
-        self.name = name
+        self.name = name.capitalize() #Forces correct format
         self.response = response
         self.topics = topics
+
+    def __str__(self):
+        string = self.name + " " + self.response
+        return string
 
 
 
